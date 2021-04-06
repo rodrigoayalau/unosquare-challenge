f51534f0be218d39e518e8a2f519842581ae9ad5
Rodrigo Ayala <ids.ayala@gmail.com>
2021-04-06 00:26:08 -0500

    requireed comments added

--- src/test/java/pages/ShopCartPage.java
+++ src/test/java/pages/ShopCartPage.java
@@ -1,16 +1,11 @@
 package pages;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import junit.framework.Assert;
--- src/test/java/scenarios/CellphoneScenario.java
+++ src/test/java/scenarios/CellphoneScenario.java
@@ -1,16 +1,14 @@
 package scenarios;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import pages.CartValidationPage;
@@ -21,19 +19,37 @@ import pages.ShopCartFlow;
 import pages.ShopCartPage;
 import setup.SetUp;
 
+/*
+ * Acceptance Criteria
+ * Accepted Languages : C#, Java or JavaScript.
+ * Use POM and Page Factory - DONE
+ * Use Assertions - DONE
+ * Have an interface implemented - DONE
+ * Have an abstract class implemented - DONE
+ * Apply Implicit and explicit wait - DONE
+
+ * Use Specflow or NUnit, in case of Java use TestNG or Cucumber, in case of JavaScript use NightWatch - DONE
+ * Use an App.config to set the URL, the Environment and your DB connection (As this is a Production App, set default value to Prod)
+ * Use a Json file to provide the data to your scripts (Search values)
+ * Implement an API call to get Personal Data (http://dummy.restapiexample.com/api/v1/employee/1) or (http://dummy.restapiexample.com/api/v1/employees)
+ * Implement a report using extend reports or a custom one
+ * Create a reusable method for DB Connection
+*/
+
 //Interface implemented for SetUp class
-public class CellphoneScenario extends SetUp{
+public class CellphoneScenario extends SetUp {
 
 	String pricePhone;
 	FileInputStream fileInput = null;
 	String cellphoneType;
 	String driverExplorer;
 	String url;
-	
-	@BeforeClass(enabled=true)
+
+	@BeforeClass(enabled = true)
 	public void setUp() throws IOException {
-		Properties properties= new Properties();
-		File file = new File("D:\\Development\\Java\\unosquare-challenge\\src\\test\\resources\\resoruces\\data.properties");
+		Properties properties = new Properties();
+		File file = new File(
+				"D:\\Development\\Java\\unosquare-challenge\\src\\test\\resources\\resoruces\\data.properties");
 		fileInput = new FileInputStream(file);
 		properties.load(fileInput);
 		// data.properties file implemented to store any kind of data
@@ -44,73 +60,75 @@ public class CellphoneScenario extends SetUp{
 		driver.get(url);
 		driver.manage().window().maximize();
 	}
-	
-	//2. Search for Samsung Galaxy Note 20.
-	@Test(priority=1, enabled=true)
+
+	// 2. Search for Samsung Galaxy Note 20.
+	@Test(priority = 1, enabled = true)
 	public void phoneSearch() {
 		SearchMainPage searchPage = new SearchMainPage(driver);
 		try {
 			// Explicit wait in the method
 			Assert.assertTrue(searchPage.searchCriteria(cellphoneType), "Failed trying to search criteria.");
 			Assert.assertTrue(searchPage.clickButtonSearch(), "Failed trying to click search button.");
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	// 3. Verify Item is displayed on the screen and  locate the first one, then store the price.
+
+	// 3. Verify Item is displayed on the screen and locate the first one, then
+	// store the price.
 	// 4. Click on the First Result.
-	@Test(priority=2, enabled=true)
+	@Test(priority = 2, enabled = true)
 	public void getPrice() {
 		FirstResultPage resultPage = new FirstResultPage(driver);
 		try {
 			this.pricePhone = resultPage.getPrice();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	// 5. Once in the details page compare this price vs the above one (first stored price).
-	@Test(priority=3, enabled=true)
+
+	// 5. Once in the details page compare this price vs the above one (first stored
+	// price).
+	@Test(priority = 3, enabled = true)
 	public void validatePrices() {
 		ProductPage galaxyPage = new ProductPage(driver);
 		try {
 			Assert.assertTrue(galaxyPage.priceValidator(pricePhone), "Prices do not match.");
 			Assert.assertTrue(galaxyPage.addToCart(), "Failed trying to add item to cart.");
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// 6. Click on Add to Cart.
-	@Test(priority=4, enabled=true)
+	@Test(priority = 4, enabled = true)
 	public void clickCartButton() {
 		// Explicit wait in the method
 		CartValidationPage validateItem = new CartValidationPage(driver);
 		try {
 			Assert.assertTrue(validateItem.clickCartButton(), "Failed trying to click cart button.");
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// 7. Go to Cart and verify again the price of the phone.
 	// 8. Delete Item.
-	@Test(priority= 5, enabled=true)
+	@Test(priority = 5, enabled = true)
 	public void shopCartPageValidation() {
-		//Abstract class
+		// Abstract class
 		ShopCartFlow validateItem = new ShopCartPage(driver);
 		try {
 			Assert.assertTrue(validateItem.priceValidation(pricePhone), "Prices do not match.");
-			//Implicit wait to validate text for empty cart
+			// Implicit await to validate text for empty cart
 			Assert.assertTrue(validateItem.deleteItem(), "Failed trying to delete item in cart.");
-			driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-		} catch(Exception e) {
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	@AfterClass(enabled=true)
+
+	@AfterClass(enabled = true)
 	public void tearDown() {
 		driver.close();
 		driver.quit();
